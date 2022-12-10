6d13a91ae22116d28bdce13b842dc82dbb08de55
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-10 23:58:37 +0100

    Section 2 - let, const and vaar

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,64 @@
+
+/*
+let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 23;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'treacher';
+
+console.log(myFirstJob);
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
