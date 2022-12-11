eb2f085b002cc851192befbfd544f2cf7e9a6357
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-11 23:46:52 +0100

    If/else statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -130,3 +130,22 @@ multiple
 lines`);
 
 */
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years`);
+}
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
