bb18f7332fc87d9eed474818e7e7b6626a31132b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-11 03:30:46 +0100

    Basic operators

--- 01-Fundamentals-Part-1/starter/assigments.js
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -28,5 +28,26 @@ console.log(typeof language);
 // 2. Think about which variables should be const variables (which values will never
 // change, and which might change?). Then, change these variables to const.
 // 3. Try to change one of the changed variables now, and observe what happens
-language = "Polish";
+language = "polish";
 // continent = "Africa";
+
+// LECTURE: Basic Operators
+// 1. If your country split in half, and each half would contain half the population,
+// then how many people would live in each half?
+// 2. Increase the population of your country by 1 and log the result to the console
+// 3. Finland has a population of 6 million. Does your country have more people than
+// Finland?
+// 4. The average population of a country is 33 million people. Does your country
+// have less people than the average country?
+// 5. Based on the variables you created, create a new variable 'description'
+// which contains a string with this format: 'Portugal is in Europe, and its 11 million
+// people speak portuguese'
+population /= 2;
+console.log(population);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+let description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
+
