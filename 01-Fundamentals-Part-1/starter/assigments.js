d4cdade4b935231bd5797b6eca019d2f8b64a615
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-13 02:07:18 +0100

    Tye conversion and type coersion

--- 01-Fundamentals-Part-1/starter/assigments.js
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -72,3 +72,18 @@ if (population > 33) {
     const populationToAverage = 33 - population;
     console.log(`${country}'s population is ${populationToAverage} million below average`);
 }
+
+// LECTURE: Type Conversion and Coercion
+// 1. Predict the result of these 5 operations without executing them:
+// '9' - '5';
+// '19' - '13' + '17';
+// '19' - '13' + 17;
+// '123' < 57;
+// 5 + 6 + '4' + 9 - 4 - 2;
+// 2. Execute the operations to check if you were right
+
+console.log('9' - '5');//4
+console.log('19' - '13' + '17');//617
+console.log('19' - '13' + 17);//23
+console.log('123' < 57);//false
+console.log(5 + 6 + '4' + 9 - 4 - 2);//1143
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -129,7 +129,7 @@ console.log(`String
 multiple
 lines`);
 
-*/
+
 
 const age = 15;
 
@@ -149,3 +149,26 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+*/
+
+//type conversion
+const inputYear = "1990";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '10');
+console.log('23' / '10');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
