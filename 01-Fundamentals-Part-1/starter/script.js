b53463478e06be32d7ae2c1f1a1f88de2d612b27
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-14 01:53:25 +0100

    Equality operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -149,7 +149,7 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
-*/
+
 
 //type conversion
 const inputYear = "1990";
@@ -172,3 +172,54 @@ let n = '1' + 1;
 n = n - 1;
 console.log(n);
 
+
+// 5 falsy values: 0, "", undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+console.log(Boolean(NaN));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+
+const money = 0;
+
+if (money) {
+    console.log("Don't spend it all :)");
+} else {
+    console.log('You should get a job');
+}
+
+let height;
+if (money) {
+    console.log("YAY! Height is defined");
+} else {
+    console.log('Height is undefined');
+}
+
+
+
+const age = 18;
+
+if (age === 18) console.log('You just become ana adult (strict');
+
+if (age == 18) console.log('You just become ana adult (loose)');
+
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof (favourite));
+
+if (favourite === 23) {
+    console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+    console.log("7 is also cool number!");
+} else if (favourite === 9) {
+    console.log("9 is also cool number!");
+} else {
+    console.log("Number is not 23 or 7");
+}
+
+if (favourite !== 23) {
+    console.log("Why not 23?");
+}
+*/
