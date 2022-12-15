b6cb1d34288ac8d6b705f81ecf66dd084b29e794
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-15 21:58:35 +0100

    Function expression vs declaration

--- 02-Fundamentals-Part-2/starter/assigments.js
+++ 02-Fundamentals-Part-2/starter/assigments.js
@@ -17,3 +17,42 @@ const thirdCountry = describeCountry("Ukraine", "50", "Kiev");
 console.log(firstCountry);
 console.log(seconfCountry);
 console.log(thirdCountry);
+
+
+// LECTURE: Function Declarations vs. Expressions
+// 1. The world population is 7900 million people. Create a function declaration
+// called 'percentageOfWorld1' which receives a 'population' value, and
+// returns the percentage of the world population that the given population
+// represents. For example, China has 1441 million people, so it's about 18.2% of
+// the world population
+// 2. To calculate the percentage, divide the given 'population' value by 7900
+// and then multiply by 100
+// 3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+// store the results into variables, and log them to the console
+// 4. Create a function expression which does the exact same thing, called
+// 'percentageOfWorld2', and also call it with 3 country populations (can be
+// the same populations)
+
+const chinaPopulation = 1441;
+const indiaPopulation = 1022;
+const polandPopulation = 40;
+
+
+function percentageOfWorld1(population) {
+    return (population / 7900 * 100);
+}
+
+const percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+}
+const chinaPercentage = percentageOfWorld1(chinaPopulation)
+const indiaPercentage = percentageOfWorld1(indiaPopulation);
+const polandPercentage = percentageOfWorld1(polandPopulation);
+
+console.log(chinaPercentage);
+console.log(indiaPercentage);
+console.log(polandPercentage);
+
+console.log(percentageOfWorld2(chinaPopulation));
+console.log(percentageOfWorld2(indiaPopulation));
+console.log(percentageOfWorld2(polandPopulation));
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,3 +34,20 @@ console.log(appleOrangeJuice);
 
 const num = Number('23')
 */
+
+//Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+
+console.log(age1);
+
+//Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age2);
