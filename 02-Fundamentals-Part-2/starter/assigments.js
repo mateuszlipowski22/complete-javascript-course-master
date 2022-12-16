0293c889051ce57f1fefb32a8ee23b9c861d9933
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-17 00:25:33 +0100

    Object methods

--- 02-Fundamentals-Part-2/starter/assigments.js
+++ 02-Fundamentals-Part-2/starter/assigments.js
@@ -160,9 +160,35 @@ console.log(myCountry);
 // decrease it by two million using brackets notation.
 
 console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries
-// and a capital called ${myCountry.capital}.`);
+and a capital called ${myCountry.capital}.`);
 
 myCountry.population = myCountry.population - 2;
 console.log(myCountry.population);
 myCountry['population'] = myCountry['population'] + 2;
 console.log(myCountry.population);
+
+
+// LECTURE: Object Methods
+// 1. Add a method called 'describe' to the 'myCountry' object. This method
+// will log a string to the console, similar to the string logged in the previous
+// assignment, but this time using the 'this' keyword.
+// 2. Call the 'describe' method
+// 3. Add a method called 'checkIsland' to the 'myCountry' object. This
+// method will set a new property on the object, called 'isIsland'.
+// 'isIsland' will be true if there are no neighbouring countries, and false if
+// there are. Use the ternary operator to set the property.
+
+myCountry.describe = function () {
+    return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries
+and a capital called ${this.capital}.`
+}
+
+console.log(myCountry.describe());
+
+myCountry.checkIsland = function () {
+    this.isIsland = this.neighbours.length === 0 ? true : false;
+    return this.isIsland;
+}
+
+myCountry.checkIsland();
+console.log(myCountry.isIsland);
