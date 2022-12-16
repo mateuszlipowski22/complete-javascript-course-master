e0ffbff38a74e12aa5fc0be7ac93bcec8d930e5f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-16 23:56:26 +0100

    dot and bracket notation

--- 02-Fundamentals-Part-2/starter/assigments.js
+++ 02-Fundamentals-Part-2/starter/assigments.js
@@ -145,7 +145,24 @@ const myCountry = {
     country: 'Poland',
     capital: 'Warsaw',
     language: 'polish',
-    population: 40
+    population: 40,
+    neighbours: ['Germany', 'Czech Republic', 'Slovakia', 'Ukraine']
 }
 
 console.log(myCountry);
+
+
+// LECTURE: Dot vs. Bracket Notation
+// 1. Using the object from the previous assignment, log a string like this to the
+// console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+// and a capital called Helsinki.'
+// 2. Increase the country's population by two million using dot notation, and then
+// decrease it by two million using brackets notation.
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries
+// and a capital called ${myCountry.capital}.`);
+
+myCountry.population = myCountry.population - 2;
+console.log(myCountry.population);
+myCountry['population'] = myCountry['population'] + 2;
+console.log(myCountry.population);
