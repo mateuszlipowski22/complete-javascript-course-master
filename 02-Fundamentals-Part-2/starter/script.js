041dcef05ffa78194426c0b2caef942f698dc623
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-17 00:25:49 +0100

    Object methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -231,3 +231,42 @@ console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his bes
 
 
 */
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    // calcAge: function (birthYear){
+    //     return 2037-birthYear;
+    // }
+
+    // calcAge: function () {
+    //     return 2037 - this.birthYear;
+    // }
+
+    calcAge: function () {
+        this.age = 2037 - this.birthYear
+        return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`
+    },
+
+};
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// console.log(jonas.['calcAge'](1991));
+
+//Challenge
+// `Jonas is a 46-year old teacher, and he has a driver's license`
+
+console.log(jonas.getSummary());
