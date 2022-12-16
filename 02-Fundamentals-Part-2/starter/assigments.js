6c7c66b27fa3f0f2f4af061af0b9b81995bdfd97
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-16 01:17:35 +0100

    Introduction to Arrays

--- 02-Fundamentals-Part-2/starter/assigments.js
+++ 02-Fundamentals-Part-2/starter/assigments.js
@@ -83,3 +83,26 @@ const describePopulation = function (country, population) {
 console.log(describePopulation("China", chinaPopulation));
 console.log(describePopulation("India", indiaPopulation));
 console.log(describePopulation("Poland", polandPopulation));
+
+
+// LECTURE: Introduction to Arrays
+// 1. Create an array containing 4 population values of 4 countries of your choice.
+// You may use the values you have been using previously. Store this array into a
+// variable called 'populations'
+// 2. Log to the console whether the array has 4 elements or not (true or false)
+// 3. Create an array called 'percentages' containing the percentages of the
+// world population for these 4 population values. Use the function
+// 'percentageOfWorld1' that you created earlier to compute the 4
+// percentage values
+
+const populations = [40, 1333, 120, 220];
+
+if (populations.length === 4) {
+    console.log(populations);
+}
+
+const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])]
+
+console.log(percentages);
+
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -105,3 +105,41 @@ console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Bob'));
 
 */
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = 'Mateusz'
+const me = [firstName, 'Lipowski', 2037 - 1991, 'student', friends]
+
+console.log(me);
+
+//Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[1]), calcAge(years[0]), calcAge(years[years.length - 1])];
