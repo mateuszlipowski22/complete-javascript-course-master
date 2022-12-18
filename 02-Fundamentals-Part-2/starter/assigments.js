deac45bf60bb13a96f489ef2175d4441b47233f2
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-18 01:45:57 +0100

    While loop

--- 02-Fundamentals-Part-2/starter/assigments.js
+++ 02-Fundamentals-Part-2/starter/assigments.js
@@ -239,3 +239,20 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
     }
 
 }
+
+// LECTURE: The while Loop
+// 1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+// but this time using a while loop (call the array 'percentages3')
+// 2. Reflect on what solution you like better for this task: the for loop or the while
+// loop?
+
+const percentages3 = [];
+
+let i = 0;
+while (i < populations.length) {
+    percentages3.push(percentageOfWorld1(populations[i]))
+    i++;
+}
+
+console.log(percentages);
+console.log(percentages3);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -340,7 +340,7 @@ for (let i = 0; i < jonasArray.length; i++) {
 
 }
 
-*/
+
 
 const jonasArray = [
     'Jonas',
@@ -360,3 +360,26 @@ for (let excercise = 1; excercise <= 3; excercise++) {
         console.log(`Excercise ${excercise} Lifting weight repetition ${rep}`);
     }
 }
+
+*/
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weigthts repetition ${rep}`);
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+//     console.log(`Lifting weigthts repetition ${rep}`);
+//     rep++
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+// while (dice !== 6) {
+//     console.log(`You rolled a ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) {
+//         console.log(`Loop is about to end...`);
+//     }
+// }
