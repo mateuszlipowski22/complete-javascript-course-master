1364633d171d4d75934ffd8bb75501a69f1d1b2b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-22 02:41:35 +0100

    Adding again button functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,9 +12,21 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+
+document.querySelector('.btn.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
