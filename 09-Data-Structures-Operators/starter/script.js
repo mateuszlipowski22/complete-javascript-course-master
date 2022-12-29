6d4644c32120415646d113bea445be3b2c67a820
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-29 02:08:27 +0100

    Spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -40,8 +40,62 @@ const restaurant = {
     console.log(`Odrder recived! ${this.starterMenu[starterIndex]}
     and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicius pasta with:
+    ${ing1}
+    ${ing2}
+    ${ing3}`);
+  },
 };
 
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// iterables: arrays, strings, maps, sets but not objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S. '];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Schmedtmann`);
+
+// const ingredients = [
+//   prompt("Ler's make pasta! Ingredient 1"),
+//   prompt('Ingredient 2'),
+//   prompt('Ingredient 3'),
+// ];
+
+// console.log(ingredients);
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+const newRestaurant = { foindedIn: 1992, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+//Destructuring Objects
+/*
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via del Sole, 32',
@@ -53,7 +107,7 @@ restaurant.orderDelivery({
   address: 'Via del Sole, 32',
 });
 
-/*
+
 const arr = [1, 2, 3];
 const a = arr[0];
 const b = arr[1];
@@ -88,7 +142,7 @@ console.log(i, j, k);
 //Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
-*/
+
 
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
@@ -118,3 +172,4 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c);
+*/
