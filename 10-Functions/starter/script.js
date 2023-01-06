421f47f570ec8096c0df12c841d8ea44e76a7930
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-06 22:34:07 +0100

    Bind method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -115,7 +115,7 @@ const greeArrSol = greeting => name => console.log(`${greeting} ${name}`);
 
 greeArrSol('Hi')('Jonas');
 
-*/
+
 
 const lufthansa = {
   airline: 'Lufthansa',
@@ -165,3 +165,56 @@ console.log(swiss);
 
 book.call(swiss, ...flightData);
 console.log(swiss);
+
+// Bind method
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(234, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schemedtmann');
+bookEW23('Martha Cooper');
+console.log(eurowings);
+
+//With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+//Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+//addVAT = value => value+value*0.23;
+console.log(addVAT(100));
+console.log(addVAT(200));
+
+const addVATFunction = value => {
+  return addTax(0.23, value);
+};
+
+console.log(addVATFunction(200));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(200));
+
+*/
