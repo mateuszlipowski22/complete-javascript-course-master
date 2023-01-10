119e7eb0431ed2f85ea4533cd26499cf054b91a7
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-11 00:06:51 +0100

    ForEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -106,7 +106,7 @@ console.log(...arr, ...arr2);
 
 // JOIN
 console.log(letters.join('-'));
-*/
+
 
 const arr = [23, 11, 64];
 console.log(arr[0]);
@@ -119,3 +119,25 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('----FOREACH----');
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
