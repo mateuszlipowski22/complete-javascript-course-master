a3cb50b29a03be0f1e94e0159780f30cd8e0c1be
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-11 21:49:59 +0100

    The filter method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -222,4 +222,23 @@ const movementsDescriptions = movements.map((mov, i) => {
 });
 
 console.log(movementsDescriptions);
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositeFor = [];
+for (const mov of movements) if (mov > 0) depositeFor.push(mov);
+
+console.log(depositeFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
 */
