eabe8b862fd8e2335bdc8f52f5219a514112ef8a
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-01-17 02:20:16 +0100

    crating and filling arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -498,3 +498,41 @@ movements.sort((a, b) => b - a);
 console.log(movements);
 
 */
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(()=>5));
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+//Array.from()
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (curr, i) => i + 1);
+const t = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+// const diceArray = Array.from(
+//   { length: 100 },
+//   () => Math.floor(Math.random() * 6) + 1
+// );
+// console.log(diceArray);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    element => Number(element.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movements2 = [...document.querySelectorAll('.movements__value')];
+  console.log(movements2);
+});
