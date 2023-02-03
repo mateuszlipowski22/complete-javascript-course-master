f86963ea4627e9bb39cd5dd4e02bf9848fa6c782
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-03 21:18:37 +0100

    Construction of function and new operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //   this.caclAge = function(){
+  //     console.log(2037-this.birthYear);
+  //   }
+};
+
+const jonas = new Person('Jonas', 1991);
+
+//1 empty object is created
+//2 function is called, this{}
+//3 {} linked to prototype
+//4 function automatically return {}
+
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(jonas instanceof Person);
