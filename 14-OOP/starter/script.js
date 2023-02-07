aa12e827522004354cac66ca4f05bb82e3a6e26b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-07 01:43:40 +0100

    Another class example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -272,7 +272,7 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
 
-*/
+
 
 const PersonProto = {
   calcAge() {
@@ -301,3 +301,44 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'COmputer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening account, ${owner}`);
+  }
+
+  //Public interface
+  deposite(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.movements.push(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoad(val) {
+    if (this.approveLoan(val)) {
+      this.deposite(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+acc1.deposite(240);
+acc1.deposite(-2240);
+acc1.requestLoad(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
