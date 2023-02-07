ceda6fc03ca2c38c2d9e8338ac05d78dff219514
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-08 00:45:41 +0100

    Challenge #4

--- 14-OOP/starter/challenge.js
+++ 14-OOP/starter/challenge.js
@@ -132,3 +132,55 @@ EV.prototype.constructor = EV;
 const tesla = new EV('Tesla', 120, 90);
 tesla.accelerate();
 tesla.break();
+
+// Coding Challenge #4
+// Your tasks:
+// 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+// child class of the 'CarCl' class
+// 2. Make the 'charge' property private
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl'
+// class. Then experiment with chaining!
+// Test data:
+// §
+// Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+// GOOD LUCK 😀
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+
+  chargeBattery(charge) {
+    this.#charge = charge;
+    return this;
+  }
+
+  break() {
+    this.speed -= 10;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+
+rivian.accelerate().chargeBattery(80).break().accelerate();
