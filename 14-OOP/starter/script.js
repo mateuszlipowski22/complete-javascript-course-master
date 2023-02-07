8d1cc06c24f79f66643b6fdff3420ab55b913260
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-02-07 01:49:33 +0100

    Protected properties and methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -307,30 +307,35 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    //protected
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
     console.log(`Thanks for opening account, ${owner}`);
   }
 
   //Public interface
   deposite(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
-    this.movements.push(-val);
+    this.deposite(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoad(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposite(val);
       console.log(`Loan approved`);
     }
+  }
+
+  getMovements() {
+    return this._movements;
   }
 }
 
@@ -342,3 +347,4 @@ acc1.requestLoad(1000);
 
 console.log(acc1);
 console.log(acc1.pin);
+console.log(acc1.getMovements());
