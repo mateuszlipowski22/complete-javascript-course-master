11b2ff1eff8be6c73924aa045dfc35913ac3bfcf
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-14 22:56:16 +0200

    Rendering the Recipe

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    if (!res.ok) throw new Error(`${data.message}  (${data.status})`);
+
+    const data = await res.json();
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(res, data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+console.log('test');
