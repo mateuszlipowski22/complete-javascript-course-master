d1c4491d75427f61b71ea2ccb9637a4d302a925f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-04-15 22:22:53 +0200

    Listening For load and hashchange Events

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -30,10 +30,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
-    renderSpinner(recipeContainer);
     // Loading data from API
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -161,4 +165,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
